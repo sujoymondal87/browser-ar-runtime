a7923a98fc4269a125e553cffea0dd7b66207017
Sujoy Kumar Mondal <sujoymondal87@gmail.com>
2026-06-30 17:21:03 +0530

    Add x offset +0.05 for hat as well

--- js/face-ar.js
+++ js/face-ar.js
@@ -159,7 +159,7 @@ const FaceAR = (() => {
     }
   }
 
-  function _getXOffset(effect) { return effect === 'hat' ? 0    : 0.05;  }
+  function _getXOffset(effect) { return effect === 'hat' ? 0.05 : 0.05;  }
   function _getYOffset(effect) { return effect === 'hat' ? 1.55 : 0.73;  }
   function _getScale(effect)   { return effect === 'hat' ? 1.0  : 6.0;   }
 
